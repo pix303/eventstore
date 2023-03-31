a1f077aa14a8adace378b84b71903e51a1e6ffcf
pix <303pix@gmail.com>
2023-03-31 22:18:52 +0200

    add temp test in main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/pix303/eventstore/store"
 )
@@ -10,10 +11,22 @@ func main() {
 
 	log.Println("init event store")
 
-	_, err := store.NewStore(store.WithBBoltRepository())
+	store, err := store.NewStore(store.WithBBoltRepository())
+	if err != nil {
+		log.Fatal(err)
+	}
+	store.Add("ADD_MESSAGE", "MSG", "123", "ciao ciao ciao")
+	time.Sleep(time.Millisecond * 1)
+	store.Add("ADD_MESSAGE", "MSG", "123", "ciao ciao")
+	time.Sleep(time.Second * 1)
+	store.Add("ADD_MESSAGE", "MSG", "123", "ciao")
+	store.Add("ADD_MESSAGE", "MSG", "443", "ciao ciao 443")
+
+	r, err := store.GetAggregate("MSG", "123")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	log.Println(len(r))
+	log.Println(r)
 }
